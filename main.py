cba47be1284b60b829cb01b16f38ccc68ddfbdc9
Jannis Kammeier <jannis.kammeier@t-online.de>
2021-12-11 00:17:07 +0100

    created Character class

--- main.py
+++ main.py
@@ -12,26 +12,14 @@ windowH = 500
 
 # colors
 colorBackground = (50, 50, 50)
+colorText = (250, 250, 255)
 
 words = ["Actor", "Gold", "Painting", "Advertisement", "Grass", "Parrot", "Afternoon", "Greece", "Pencil", "Airport", "Guitar", "Piano", "Ambulance", "Hair", "Pillow", "Animal", "Hamburger", "Pizza", "Answer", "Helicopter", "Planet", "Apple", "Helmet", "Plastic", "Army", "Holiday", "Portugal", "Australia", "Honey", "Potato", "Balloon", "Horse", "Queen", "Banana", "Hospital", "Quill", "Battery", "House", "Rain", "Beach", "Hydrogen", "Rainbow", "Beard", "Ice", "Raincoat", "Bed", "Insect", "Refrigerator", "Belgium", "Insurance", "Restaurant", "Boy", "Iron", "River", "Branch", "Island", "Rocket", "Breakfast", "Jackal", "Room", "Brother", "Jelly", "Rose", "Camera", "Jewellery", "Russia", "Candle", "Jordan", "Sandwich", "Car", "Juice", "School", "Caravan", "Kangaroo", "Scooter", "Carpet", "King", "Shampoo", "Cartoon", "Kitchen", "Shoe", "China", "Kite", "Soccer", "Church", "Knife", "Spoon", "Crayon", "Lamp", "Stone", "Crowd", "Lawyer", "Sugar", "Daughter", "Leather", "Sweden", "Death", "Library", "Teacher", "Denmark", "Lighter", "Telephone", "Diamond", "Lion", "Television", "Dinner", "Lizard", "Tent", "Disease", "Lock", "Thailand", "Doctor", "London", "Tomato", "Dog", "Lunch", "Toothbrush", "Dream", "Machine", "Traffic", "Dress", "Magazine", "Train", "Easter", "Magician", "Truck", "Egg", "Manchester", "Uganda", "Eggplant", "Market", "Umbrella", "Egypt", "Match", "Van", "Microphone", "Vase", "Energy", "Monkey", "Vegetable", "Engine", "Morning", "Vulture", "England", "Motorcycle", "Wall", "Evening", "Nail", "Whale", "Eye", "Napkin", "Window", "Family", "Needle", "Wire", "Finland", "Nest", "Xylophone", "Fish", "Nigeria", "Yacht", "Flag", "Night", "Yak", "Flower", "Notebook", "Zebra", "Football", "Ocean", "Zoo", "Forest", "Oil", "Garden", "Fountain", "Orange", "Gas", "France", "Oxygen", "Girl", "Furniture", "Oyster", "Glass", "Garage", "Ghost"]
 
-mainFont = pygame.font.SysFont('sans-serif', 18)
+mainFont = pygame.font.SysFont('sans-serif', 30)
 
 window = pygame.display.set_mode((windowW, windowH))
 pygame.display.set_caption("Hangman")
-
-running = True
-
-while running:
-    window.fill(colorBackground)
-    for event in pygame.event.get():
-        if event == pygame.QUIT:
-            pygame.font.quit()
-            pygame.quit()
-            quit()
-
-    pygame.display.update()
-    clock.tick(60)
 
 
 def getRandomWord():
@@ -48,3 +36,39 @@ def getCharSpot(char, word):
     else:
         return -1
 
+
+class Character:
+    def __init__(self, value, position, visible=False):
+        self.value = value
+        self.position = position
+        self.visible = visible
+
+    def getValue(self):
+        return self.value
+
+    def getPosition(self):
+        return self.position
+
+    def getVisible(self):
+        return self.visible
+
+    def setVisible(self, visible=True):
+        self.visible = visible
+
+
+word = getRandomWord()
+testRender = mainFont.render(word, True, colorText)
+
+running = True
+while running:
+    window.fill(colorBackground)
+    for event in pygame.event.get():
+        if event == pygame.QUIT:
+            pygame.font.quit()
+            pygame.quit()
+            quit()
+
+    window.blit(testRender, (0, 0))
+
+    pygame.display.update()
+    clock.tick(60)
